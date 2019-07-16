faec861f43d16d81cfe60def8d2c4cbe283de8d4
maggieezzat <maggieezzat9@gmail.com>
2019-07-16 11:13:51 +0200

    script to run

---
+++ build_vocab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python subword_builder.py \
+--corpus_filepattern /lm_corpus/dewiki_nltk_segmented/*/*.txt \
+--output_filename /bert/bert-vocab-builder/vocab/dewiki_limited_chars_500.txt \
+--min_count 500 \
+--corpus_max_lines 100000000\
+--num_iterations 5\
+--split_on_newlines True \
+--do_lower True
