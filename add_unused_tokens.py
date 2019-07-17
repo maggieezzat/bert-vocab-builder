e557fe674e9409a461247777e4b8423d40db6836
maggieezzat <maggieezzat9@gmail.com>
2019-07-17 14:31:51 +0200

    unused

---
+++ add_unused_tokens.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+from os import listdir, remove
+from os.path import isfile, join
+import sys
+import unicodedata
+
+import string
+import collections
+
+vocab_file = "/bert/bert-vocab-builder/vocab/dewiki_limited_chars_100.txt"
+
+def add_unused(vocab=vocab_file, n=1000):
+
+    with open(vocab, 'r+') as vf:
+        lines = vf.readlines()
+        for i in range(0, n):
+            vf.write("[unused" + str(i) + "]")
+
+
+def main():
+    add_unused()
+
+
+if __name__ == "__main__":
+    main()
+
