03b38af41a3ab0377950d531f0baedf35c0a4b5c
maggieezzat <maggieezzat9@gmail.com>
2019-07-17 11:26:48 +0000

    vocab files

--- build_vocab.sh
+++ build_vocab.sh
@@ -2,9 +2,9 @@
 
 python subword_builder.py \
 --corpus_filepattern "/lm_corpus/dewiki_nltk_segmented/*/*.txt" \
---output_filename "/bert/bert-vocab-builder/vocab/dewiki_limited_chars_500_30it_no_extra_chars.txt" \
---min_count 500 \
+--output_filename "/bert/bert-vocab-builder/vocab/dewiki_limited_chars_900.txt" \
+--min_count 900 \
 --corpus_max_lines 1000000000 \
---num_iterations 30 \
+--num_iterations 10 \
 --split_on_newlines True \
 --do_lower True
